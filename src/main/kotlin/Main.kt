0b75d929241650ed5cb20e9f90103a7be7fb3c05
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-10 22:38:07 +0300

    remove unused import

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
